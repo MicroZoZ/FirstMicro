a27ce6a384924e5400002e5053a2427f9a4729e1
MicroZoZ <153441280+MicroZoZ@users.noreply.github.com>
2024-05-20 01:23:15 +0300

    Login And Signup Commitment

---
+++ resources/views/Masterpages/authmaster.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @extends("layouts.authlayouts.header")
+</head>
+<!--end::Head-->
+<!--begin::Body-->
+
+<body id="kt_body" class="app-blank app-blank">
+    <!--begin::Theme mode setup on page load-->
+    <script>
+    var defaultThemeMode = "light";
+    var themeMode;
+    if (document.documentElement) {
+        if (document.documentElement.hasAttribute("data-bs-theme-mode")) {
+            themeMode = document.documentElement.getAttribute("data-bs-theme-mode");
+        } else {
+            if (localStorage.getItem("data-bs-theme") !== null) {
+                themeMode = localStorage.getItem("data-bs-theme");
+            } else {
+                themeMode = defaultThemeMode;
+            }
+        }
+        if (themeMode === "system") {
+            themeMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+        }
+        document.documentElement.setAttribute("data-bs-theme", themeMode);
+    }
+    </script>
+    <!--end::Theme mode setup on page load-->
+    <!--begin::Root-->
+    <div class="d-flex flex-column flex-root" id="kt_app_root">
+        <!--begin::Authentication - Sign-up -->
+        <div class="d-flex flex-column flex-lg-row flex-column-fluid">
+            <!--begin::Aside-->
+            <div class="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center"
+                style="background-image: url(assets/media/misc/auth-bg.png)">
+                <!--begin::Content-->
+                <div class="d-flex flex-column flex-center p-6 p-lg-10 w-100">
+                    <!--begin::Logo-->
+                    <a href="../../demo1/dist/index.html" class="mb-0 mb-lg-20">
+                        <img alt="Logo" src="{{asset("assets/media/logos/default-white.svg")}}"
+                            class="h-40px h-lg-50px" />
+                    </a>
+                    <!--end::Logo-->
+                    <!--begin::Image-->
+                    <img class="d-none d-lg-block mx-auto w-300px w-lg-75 w-xl-500px mb-10 mb-lg-20"
+                        src="{{asset("assets/media/misc/auth-screens.png")}}" alt="" />
+                    <!--end::Image-->
+                    <!--begin::Title-->
+                    <h1 class="d-none d-lg-block text-white fs-2qx fw-bold text-center mb-7">Fast, Efficient and
+                        Productive</h1>
+                    <!--end::Title-->
+                    <!--begin::Text-->
+                    <div class="d-none d-lg-block text-white fs-base text-center">In this kind of post,
+                        <a href="#" class="opacity-75-hover text-warning fw-semibold me-1">the blogger</a>introduces a
+                        person they’ve interviewed
+                        <br />and provides some background information about
+                        <a href="#" class="opacity-75-hover text-warning fw-semibold me-1">the interviewee</a>and their
+                        <br />work following this is a transcript of the interview.
+                    </div>
+                    <!--end::Text-->
+                </div>
+                <!--end::Content-->
+            </div>
+            <!--end::Aside-->
+            @yield("form_body")
+        </div>
+        <!--end::Authentication - Sign-up-->
+    </div>
+    <!--end::Root-->
+    <!--begin::Javascript-->
+    @extends("layouts.authlayouts.scripts")
+</body>
+<!--end::Body-->
+
+</html>
---
+++ resources/views/layouts/authlayouts/scripts.blade.php
@@ -0,0 +1,9 @@
+<script>
+var hostUrl = "assets/";
+</script>
+<!--begin::Global Javascript Bundle(mandatory for all pages)-->
+<script src="{{asset("assets/plugins/global/plugins.bundle.js")}}"></script>
+<script src="{{asset("assets/js/scripts.bundle.js")}}"></script>
+<!--end::Global Javascript Bundle-->
+@yield("scripts")
+<!--end::Javascript--
